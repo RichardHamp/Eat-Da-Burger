f071404a42c622cb5b125c7930830602babe1c50
fozzie77 <54290959+fozzie77@users.noreply.github.com>
2020-01-16 11:41:13 -0700

    Structure of both .handlebars files complete

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name) VALUES ("Vanilla Icecream Burger With Chives");
-INSERT INTO burgers (burger_name) VALUES ("Hamburgler");
-INSERT INTO burgers (burger_name) VALUES ("Open-Faced...I should stop there. No one wants it already.");
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Vanilla Icecream Burger With Chives", false),("Hamburgler", false),("Open-Faced...I should stop there. No one wants it already.", false)
--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 var express = require("express");
+var expressHandlebars = require("express-handlebars");
+// var bodyParser = require("body-parser");
+
+var app = express();
+var PORT = process.env.PORT || 3030;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+app.engine("handlebars", expressHandlebars({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
+app.listen(PORT, function(){
+    console.log("Server listening on: http://localhost:"+PORT);
+});
