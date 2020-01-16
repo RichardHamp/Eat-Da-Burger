c272568d23100a7a1af8a58eb6001831b98af108
fozzie77 <54290959+fozzie77@users.noreply.github.com>
2020-01-15 18:19:02 -0700

    Wrote both .sql files (schema and seeds)

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Vanilla Icecream Burger With Chives");
+INSERT INTO burgers (burger_name) VALUES ("Hamburgler");
+INSERT INTO burgers (burger_name) VALUES ("Open-Faced...I should stop there. No one wants it already.");
