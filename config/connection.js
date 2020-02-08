c3518d9e3b2b34a357bb106ed1b4d08eb5e49f5b
fozzie77 <54290959+fozzie77@users.noreply.github.com>
2020-02-08 03:33:08 -0700

    reworking connections

--- config/connection.js
+++ config/connection.js
@@ -2,24 +2,30 @@
 const mysql = require("mysql");
 
 //Connection with requirements for Heroku
-var connection;
+// var connection;
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  console.log("process.env.JAWSDB_URL = " + process.env.JAWSDB_URL);
+} 
+else {
  connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "password",
-  database: "burgers_db"
+  database: "burgers_db",
+  // port: '3309'
 });
 };
 
+// connection.connect();
+
 //Throws error or gives connected thread id
 connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
   
 module.exports = connection;
