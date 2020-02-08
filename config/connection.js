0c15dc376d85c2057dccd55da7b65190e91ec45c
fozzie77 <54290959+fozzie77@users.noreply.github.com>
2020-02-08 03:41:37 -0700

    adding console.logs to determine connection issues

--- config/connection.js
+++ config/connection.js
@@ -27,5 +27,12 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+
+console.log({
+  host            : process.env.MYSQL_HOST,
+  user            : process.env.MYSQL_USER,
+  password        : process.env.MYSQL_SECRET,
+  database        : process.env.MYSQL_DB
+})
   
 module.exports = connection;
